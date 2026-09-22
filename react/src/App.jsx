759123bb4ad48ae6f5119a642f84766776f9486d
KarlEstante-20111872 <20111872@mail.wit.ie>
2026-09-22 12:03:32 +0100

    Delete a task

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -5,11 +5,22 @@ import React, { useState } from 'react';
 function App() {
       const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", deadline: "Today" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority:"High",done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority:"Medium", done: false },
+      { id: 3, title: "Tidy up", deadline: "Today", priority:"Low", done: false }
     ]
   });
+    const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({tasks});
+    console.log(`${taskIndex} ${tasks[taskIndex].done}`);
+  }
+    const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
     return (
     <div className="container">
       <h1>Tasky</h1>
@@ -18,12 +29,15 @@ function App() {
       title={task.title}
       description={task.description}
       deadline={task.deadline}
+      priority={task.priority}
       key={task.id}
+      markDone={() => doneHandler(index)}
+      deleteTask = {() => deleteHandler(index)}
     />
+    
   ))} 
     </div>
   );
-
 }
 
 export default App;
--- react/src/components/Task.jsx
+++ react/src/components/Task.jsx
@@ -4,7 +4,11 @@ const Task = (props) => {
         <div className="card">
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
+            <p>{props.description}</p>
+            <p>{props.priority}</p>
             <p>{props.children}</p>
+            <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
+            <button onClick={props.markDone} className='doneButton'>Done</button>
         </div>
     )
 }
